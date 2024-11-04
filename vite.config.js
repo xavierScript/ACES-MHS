a137da57347dff3892b80a88d6c24a00e1f60356
obuhdaniel <danielobuh05@gmail.com>
2024-11-04 09:18:03 +0100

    Fix: vite config to achieve routing for SPA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { copyFileSync } from 'fs';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/ACES-MHS/",
-})
+  base: '/ACES-MHS/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        
+      },
+    },
+  },
+  plugins: [
+    {
+      name: 'copy-404',
+      generateBundle() {
+        
+        copyFileSync('dist/index.html', 'dist/404.html');
+      },
+    },
+  ],
+});
