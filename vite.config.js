37afca6795f07b06d9a24cb619e7b1552ef7c0bd
obuhdaniel <danielobuh05@gmail.com>
2024-11-04 09:27:19 +0100

    retry fix

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { copyFileSync } from 'fs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/ACES-MHS/', 
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        
-      },
-    },
-  },
-  plugins: [
-    {
-      name: 'copy-404',
-      generateBundle() {
-        
-        copyFileSync('dist/index.html', 'dist/404.html');
-      },
-    },
-  ],
-});
+  plugins: [react()],
+  base: "/ACES-MHS/",
+  
+})
